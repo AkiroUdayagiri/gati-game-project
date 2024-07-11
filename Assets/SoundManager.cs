0232212fc5a5e7c6d8cd7f421e2fcd52cb27428f
ntlouie <ntlouie@uci.edu>
2024-07-11 00:05:39 -0700

    MainMenu
    I created a main menu as well as options menu that allows you to control the volume via a slider and exit the game as well as continue into the actual game

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+
+
+    [SerializeField] Slider volumeSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("MasterVolume"))
+        {
+            PlayerPrefs.SetFloat("MasterVolume", 1);
+            Load();
+        }
+
+        else
+        {
+            Load();
+        }
+                
+                    
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+    }
+
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("MasterVolume", volumeSlider.value);
+    }
+}
